ee8dc58648af02517f2b2c2a0ed7e0128e418cf2
Chandan Singh <pawarchandan301@gmail.com>
2025-07-07 21:14:16 +0530

    update configfiles

--- config/db.js
+++ config/db.js
@@ -4,6 +4,10 @@ const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGO_URI);
     console.log(`MongoDB connected to: ${conn.connection.name}`);
+    console.log(
+      "Mongo URI from env:",
+      process.env.MONGO_URI || process.env.MONGODB_URI
+    );
   } catch (err) {
     console.error("MongoDB connection error:", err);
     process.exit(1);
