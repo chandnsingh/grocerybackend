3af3baed56855754211381ce58348a8495118518
Chandan Singh <pawarchandan301@gmail.com>
2025-07-07 21:19:49 +0530

    update dbfile

--- config/db.js
+++ config/db.js
@@ -2,12 +2,16 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI);
-    console.log(`MongoDB connected to: ${conn.connection.name}`);
     console.log(
       "Mongo URI from env:",
       process.env.MONGO_URI || process.env.MONGODB_URI
     );
+
+    const uri = process.env.MONGO_URI || process.env.MONGODB_URI;
+    if (!uri) throw new Error("MongoDB URI not found in environment variables");
+
+    const conn = await mongoose.connect(uri);
+    console.log(`MongoDB connected to: ${conn.connection.name}`);
   } catch (err) {
     console.error("MongoDB connection error:", err);
     process.exit(1);
